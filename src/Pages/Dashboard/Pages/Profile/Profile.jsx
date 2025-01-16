c6ec7a263c40e5b4e7142f88f30c9e90c949c02b
s-h-rumon <shahidhasanruman3@gmil.com>
2025-01-17 00:04:11 +0600

    profile done

--- src/Pages/Dashboard/Pages/Profile/Profile.jsx
+++ src/Pages/Dashboard/Pages/Profile/Profile.jsx
@@ -1,13 +1,44 @@
+import { useQuery } from '@tanstack/react-query';
 import React, { useEffect } from 'react';
+import useAxiosSecure from '../../../../Hooks/useAxiosSecure';
+import DashboardContainer from '../../Components/Container/DashboardContainer';
 
 const Profile = () => {
+  const axiosSecure = useAxiosSecure();
+
+  const { data: profileInfo = {} } = useQuery({
+    queryKey: ['profileInfo'],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get('/user_profile');
+      return data;
+    },
+  });
+
+  const { _id, email, displayName, role, photoURL } = profileInfo;
+
   useEffect(() => {
     document.title = 'Profile | EduHive';
   }, []);
 
   return (
     <div>
-      <p className="text-3xl font-semibold">Profile</p>
+      <DashboardContainer>
+        <section>
+          <p className="text-3xl font-semibold">Profile</p>
+
+          <div>
+            <img
+              className="max-w-48 aspect-square object-cover rounded-full"
+              src={photoURL}
+              alt=""
+            />
+            <p>Role : {role}</p>
+            <p>Name : {displayName}</p>
+            <p>Email : {email}</p>
+            <p>Number : {_id.slice(0, 15)}</p>
+          </div>
+        </section>
+      </DashboardContainer>
     </div>
   );
 };
